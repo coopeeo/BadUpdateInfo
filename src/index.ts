35de2f357e3d1509d18ced86270bd856e41b6576
Cooper <coop@coopeeo.xyz>
2025-04-10 08:44:58 -0500

    test more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import _ from 'lodash';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
